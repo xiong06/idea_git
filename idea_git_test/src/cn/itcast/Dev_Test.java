12058c3f9f56df9adca2324ac1867355cdb1c74a
Mr_Xiong <Mr_Xiong@qq.com>
2018-12-01 14:45:11 +0800

    dev分支增加一个类

---
+++ idea_git_test/src/cn/itcast/Dev_Test.java
@@ -0,0 +1,13 @@
+package cn.itcast;
+
+/**
+ * @author Mr.Xiong
+ * @date 2018/12/01 14:44
+ */
+public class Dev_Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("dev分支");
+    }
+}
