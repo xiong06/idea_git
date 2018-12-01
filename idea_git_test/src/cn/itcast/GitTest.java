58f5a518bb1357749cb4c43d99da25e386d631e0
Mr_Xiong <Mr_Xiong@qq.com>
2018-12-01 14:21:55 +0800

    添加git_test工程

---
+++ idea_git_test/src/cn/itcast/GitTest.java
@@ -0,0 +1,14 @@
+package cn.itcast;
+
+/**
+ * @author Mr.Xiong
+ * @date 2018/12/01 14:18
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("git test");
+        System.out.println("git test");
+        System.out.println("git test");
+    }
+}
